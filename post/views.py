f65acb3540717587d07ceb3ea8b581e2ad60f965
antonymwakiacha <tonywugoma@gmail.com>
2022-06-01 10:49:08 +0300

    added newpost functionality for creating new posts

--- post/views.py
+++ post/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 #from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import login_required
-from post.models import Post, Stream
+from post.models import Post, Stream, Tag
+from post.forms import NewPostForm
 
 
 @login_required
@@ -26,3 +27,36 @@ def index(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+@login_required
+def NewPost(request):
+    user = request.user.id
+    tags_objs=[]
+
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.cleaned_data.get('picture')
+            caption = form.cleaned_data.get('caption')
+            tags_form = form .cleaned_data.get('tags')
+
+            tags_list = list(tags_form.split(','))
+
+            for tag in tags_list:
+                t, created = Tag.objects.get_or_create(title=tag)
+                tags_objs.append(t)
+
+            p, created = Post.objects.get_or_create(picture=picture, caption=caption, user_id=user)
+            p.tags.set(tags_objs)
+            p.save()
+            return redirect('index')
+    else:
+        form = NewPostForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'newpost.html', context)
+
+
