93ce2b8fd0aefe8bc43942a57f6379215b3e3af0
antonymwakiacha <tonywugoma@gmail.com>
2022-06-06 16:03:51 +0300

    added the 'editprofile' feature to a user account

--- authy/models.py
+++ authy/models.py
@@ -5,6 +5,10 @@ from post.models import Post
 from django.db.models.signals import post_save
 
 # Create your models here.
+def user_directory_path(instance, filename):
+    # this file will be uploaded to MEDIA_ROOT /user(id)/flename
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	first_name = models.CharField(max_length=50, null=True, blank=True)
@@ -14,7 +18,7 @@ class Profile(models.Model):
 	profile_info = models.TextField(max_length=150, null=True, blank=True)
 	created = models.DateField(auto_now_add=True)
 	favorites = models.ManyToManyField(Post)
-	picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True, verbose_name='Picture')
+	picture = models.ImageField(upload_to=user_directory_path, blank=True, null=True, verbose_name='Picture')
 
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
